d1172637367d589ca40807b007abd6c7325d99bf
login-k <dmd13571987882@163.com>
2019-07-24 11:27:57 +0800

    add hello-world

---
+++ hellodemo/src/hellodemo/Hello.java
@@ -0,0 +1,11 @@
+package hellodemo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ÄãºÃ£¬ÊÀ½ç");
+		System.out.println("hello world");
+	}
+
+}
